028e3995eafdaf8acd3e9c799bda1e6527a79a74
jpc0701 <347706805@qq.com>
2018-01-09 20:34:08 +0800

    Add files via upload

---
+++ snake.cpp
@@ -0,0 +1,116 @@
+#include"snake.h"
+#include"tools.h"
+#include<iostream>
+#include<conio.h>
+
+void Snake::InitSnake(){
+	for(auto& point:snake){
+		point.PrintCircular();
+	}
+}
+
+void Snake::Move(){
+	switch(direction){
+		case Direction::UP:
+			snake.emplace_back(Point(snake.back().GetX(),snake.back().GetY()-1));
+			break;
+		case Direction::DOWN:
+			snake.emplace_back(Point(snake.back().GetX(),snake.back().GetY()+1));
+			break;
+		case Direction::LEFT:
+			snake.emplace_back(Point(snake.back().GetX()-1,snake.back().GetY()));
+			break;
+		case Direction::RIGHT:
+			snake.emplace_back(Point(snake.back().GetX()+1,snake.back().GetY()));
+			break;
+		default:
+			break;
+	}
+	SetColor(14);
+	snake.back().PrintCircular();
+}
+
+void Snake::NormalMove(){
+	Move();
+	snake.front().Clear();
+	snake.pop_front();
+}
+
+bool Snake::OverEdge(){
+	return snake.back().GetX()<30 && snake.back().GetY()<30 && snake.back().GetX()>1 && snake.back().GetY()>1;
+}
+
+bool Snake:: HitItself(){
+	std::deque<Point>::size_type cnt=1;
+	Point *head=new Point(snake.back().GetX(),snake.back().GetY());
+	for(auto&point:snake){
+		if(!(point==*head))
+			++cnt;
+		else
+			break;
+	}
+	delete head;
+	if(cnt==snake.size())
+		return true;
+	else
+		return false;
+}
+
+bool Snake::ChangeDirection(){
+	char ch;
+	if(kbhit()){
+		ch=getch();
+		switch(ch){
+			case -32:
+				ch=getch();
+				switch(ch){
+					case 72:
+						if(direction!=Direction::DOWN)
+							direction=Direction::UP;
+						break;
+					case 80:
+						if(direction!=Direction::UP)
+							direction=Direction::DOWN;
+						break;
+					case 75:
+						if(direction!=Direction::RIGHT)
+							direction=Direction::LEFT;
+						break;
+					case 77:
+						if(direction!=Direction::LEFT)
+							direction=Direction::RIGHT;
+						break;
+					default:
+						break;
+				}
+				return true;
+			case 27:
+				return false;
+			default:
+				return true;
+		}
+	}
+	return true;
+}
+
+bool Snake::GetFood(const Food& cfood){
+	if(snake.back().GetX()==cfood.x && snake.back().GetY()==cfood.y)
+		return true;
+	else
+		return false;
+}
+
+bool Snake::GetBigFood(Food& cfood){
+	if(snake.back().GetX()==cfood.big_x && snake.back().GetY()==cfood.big_y){
+		cfood.big_flag=false;
+		cfood.big_x=0;
+		cfood.big_y=0;
+		SetCursorPosition(1,0);
+		std::cout<<"                                                            ";
+		return true;
+	}else
+		return false;
+}
+
+
+
---
+++ startinterface.cpp
@@ -0,0 +1,53 @@
+#include"startinterface.h"
+#include<windows.h>
+
+void StartInterface::PrintFirst(){
+	for(auto&point:startsnake){
+		point.Print();
+		Sleep(speed);
+	}
+}
+
+void StartInterface::PrintSecond(){
+	for(int i=10;i!=40;++i){
+		int j=(((i-2)%8)<4)?(15+(i-2)%8):(21-(i-2)%8);
+		startsnake.emplace_back(Point(i,j));
+		startsnake.back().Print();
+		startsnake.front().Clear();
+		startsnake.pop_front();
+		Sleep(speed);
+	}
+}
+
+void StartInterface::PrintText(){
+	for(auto&point:textsnake){
+		if(point.GetX()>=0)
+			point.Print();
+	}
+}
+
+void StartInterface::ClearText(){
+	for(auto&point:textsnake){
+		if(point.GetX()>=0)
+			point.Clear();
+		point.ChangePosition(point.GetX()+1,point.GetY());
+	}
+}
+
+void StartInterface::PrintThird(){
+	while(!startsnake.empty() || textsnake.back().GetX()<33){
+		if(!startsnake.empty()){
+			startsnake.front().Clear();
+			startsnake.pop_front();
+		}
+		ClearText();
+		PrintText();
+		Sleep(speed);
+	}
+}
+
+void StartInterface::Action(){
+	PrintFirst();
+	PrintSecond();
+	PrintThird();
+}
